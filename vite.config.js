d79a841b4003883dae79a5ad8bc66f45d6e1bd66
tnii0006 <tnii0006@student.monash.edu>
2025-08-10 15:25:59 +0800

    Trigger deployment

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: import.meta.env.MODE === 'production'
+    ? '/-FIT5032_2025_Tianqi_Ni_35524456/' // 请将 <repository-name> 替换成你的仓库名
+    : '/',
   plugins: [
     vue(),
     vueDevTools(),
