9b096b723a479e992786c4943fdcb0755844201c
tnii0006 <tnii0006@student.monash.edu>
2025-08-12 17:11:36 +0800

    Trigger deployment

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
+// import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+// import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-    base: import.meta.env.MODE === 'production'
-    ? '/-FIT5032_2025_Tianqi_Ni_35524456/' // 请将 <repository-name> 替换成你的仓库名
-    : '/',
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     vue(),
+//     vueDevTools(),
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   }
+// })
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+/* eslint-env node */
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/-FIT5032_2025_Tianqi_Ni_35524456/'
+    : '/'
+}
